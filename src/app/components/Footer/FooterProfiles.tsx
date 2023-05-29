ca3e4fec3a5ec4ea913ff56987e15391d7c56c1c
iinfin <han@iinf.in>
2023-05-29 01:34:38 +0100

    fix(app): profile links

--- src/app/components/Footer/FooterProfiles.tsx
+++ src/app/components/Footer/FooterProfiles.tsx
@@ -7,22 +7,22 @@ export default function FooterProfiles(): ReactElement {
 			<span className="flex-grow text-right sm:text-left md:flex-grow-0">
 				<ul>
 					<li>
-						<a target="_blank" href="http://linkedin.com/in/u29dc">
+						<a target="_blank" href="https://linkedin.com/in/u29dc">
 							LINKEDIN
 						</a>
 					</li>
 					<li>
-						<a target="_blank" href="http://twitter.com/u29dc">
+						<a target="_blank" href="https://twitter.com/u29dc">
 							TWITTER
 						</a>
 					</li>
 					<li>
-						<a target="_blank" href="http://instagram.com/u29dc">
+						<a target="_blank" href="https://instagram.com/u29dc">
 							INSTAGRAM
 						</a>
 					</li>
 					<li>
-						<a target="_blank" href="http://vimeo.com/u29dc">
+						<a target="_blank" href="https://vimeo.com/u29dc">
 							VIMEO
 						</a>
 					</li>
