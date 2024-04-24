fe4f0bdcc311267fd712d6e8f001322f1a1fae38
iinfin <han@iinf.in>
2024-04-24 09:01:53 +0100

    feat(app): optimise images

--- src/app/components/Preload.tsx
+++ src/app/components/Preload.tsx
@@ -3,7 +3,7 @@
 import ReactDOM from 'react-dom';
 
 export default function Preload() {
-	// ReactDOM.preload('https://cdn.u29dc.com/', { as: '...' });
+	ReactDOM.preload('https://u29dc.com/assets/images/noise.webp', { as: 'image' });
 	ReactDOM.preconnect('https://cdn.u29dc.com/', { crossOrigin: 'anonymous' });
 	ReactDOM.prefetchDNS('https://cdn.u29dc.com/');
 
