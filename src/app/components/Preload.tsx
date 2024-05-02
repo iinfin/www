f1571dbb89b691414748ebc76a99bf7df7cd3168
iinfin <han@iinf.in>
2024-05-02 16:04:41 +0100

    feat(app): preload fonts

--- src/app/components/Preload.tsx
+++ src/app/components/Preload.tsx
@@ -3,7 +3,10 @@
 import ReactDOM from 'react-dom';
 
 export default function Preload() {
-	ReactDOM.preload('https://u29dc.com/assets/images/noise.webp', { as: 'image' });
+	ReactDOM.preload('/assets/images/noise.webp', { as: 'image' });
+	ReactDOM.preload('/assets/fonts/PPNeueMontreal/PPNeueMontreal-Medium.woff2', { as: 'font', type: 'font/woff2' });
+	ReactDOM.preload('/assets/fonts/PPNeueMontreal/PPNeueMontreal-Medium.woff', { as: 'font', type: 'font/woff' });
+	ReactDOM.preload('/assets/fonts/PPNeueMontreal/PPNeueMontreal-Medium.otf', { as: 'font', type: 'font/otf' });
 	ReactDOM.preconnect('https://cdn.u29dc.com/', { crossOrigin: 'anonymous' });
 	ReactDOM.prefetchDNS('https://cdn.u29dc.com/');
 
