71a217c7410992357547f408b439713f6a764cd1
iinfin <han@u29dc.com>
2024-11-08 10:02:02 +0000

    refactor(app): headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import type { NextFetchEvent, NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export function generateCsp() {
-	// const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
+	const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
 	const cspObj = [
 		{ name: 'base-uri', values: ["'self'"] },
 		{ name: 'connect-src', values: ["'self'", '*.vercel-insights.com'] },
@@ -15,31 +15,45 @@ export function generateCsp() {
 		{ name: 'img-src', values: ["'self'", 'data:', 'cdn.u29dc.com'] },
 		{ name: 'manifest-src', values: ["'self'"] },
 		{ name: 'media-src', values: ["'self'", 'cdn.u29dc.com'] },
-		{ name: 'style-src', values: ["'report-sample'", "'self'", "'unsafe-inline'"] },
-		{ name: 'script-src', values: ["'report-sample'", "'self'", "'unsafe-inline'"] },
+
+		{ name: 'style-src', values: ["'report-sample'", "'self'" /* "'unsafe-inline'" */, `'nonce-${nonce}'`] },
+		{ name: 'script-src', values: ["'report-sample'", "'self'", "'unsafe-inline'", `'nonce-${nonce}'`, "'strict-dynamic'", 'http:', 'https:'] },
 	];
 
 	const cspHeader = cspObj
-		.map((directive) => {
-			return `${directive.name} ${directive.values.join(' ')}`;
-		})
+		.map((directive) => `${directive.name} ${directive.values.join(' ')}`)
 		.join('; ')
-		.concat('; upgrade-insecure-requests')
-		.replace(/\s{2,}/g, ' ')
+		.concat('; upgrade-insecure-requests') // Correct per documentation
 		.trim();
 
-	return { cspHeader };
+	return { cspHeader, nonce };
 }
 
 export function middleware(_req: NextRequest, _fetch: NextFetchEvent) {
-	const { cspHeader } = generateCsp();
+	const { cspHeader, nonce } = generateCsp();
 
 	const mwRequestHeaders = new Headers(_req.headers);
 	mwRequestHeaders.set('content-security-policy', cspHeader);
-	// mwRequestHeaders.set('x-nonce', nonce);
+	mwRequestHeaders.set('x-nonce', nonce);
 
 	const mwResponse = NextResponse.next({ request: { headers: mwRequestHeaders } });
+	const path = _req.nextUrl.pathname;
+
+	// Apply Content Security Policy header
 	mwResponse.headers.set('content-security-policy', cspHeader);
+
+	// Apply headers conditionally based on path
+	if (!path.startsWith('/api')) {
+		mwResponse.headers.set('x-xss-protection', '1; mode=block');
+		mwResponse.headers.set('x-frame-options', 'deny');
+		mwResponse.headers.set('permissions-policy', 'fullscreen=(self)');
+	}
+
+	// Apply global headers
+	mwResponse.headers.set('strict-transport-security', 'max-age=31557600; includeSubDomains; preload');
+	mwResponse.headers.set('x-content-type-options', 'nosniff');
+	mwResponse.headers.set('referrer-policy', 'same-origin');
+	mwResponse.headers.set('vary', 'Accept-Encoding');
 
 	return mwResponse;
 }
@@ -47,11 +61,10 @@ export function middleware(_req: NextRequest, _fetch: NextFetchEvent) {
 export const config = {
 	matcher: [
 		/*
-		 * Match all request paths except for the ones starting with:
-		 * - api (API routes)
-		 * - _next/static (static files)
-		 * - _next/image (image optimization files)
-		 * - favicon.ico (favicon file)
+		 * Match all request paths except for:
+		 * - API routes: (/api)
+		 * - Static assets: (_next/static, _next/image)
+		 * - Favicons: (favicon.ico)
 		 */
 		'/((?!api|_next/static|_next/image|favicon.ico).*)',
 	],
