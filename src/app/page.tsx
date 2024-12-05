3000f88f8622e75fc8a927b8422e86618fffbce7
iinfin <han@u29dc.com>
2024-12-05 19:06:18 +0000

    fix(app): csp nonce

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-// import { headers } from 'next/headers';
+import { headers } from 'next/headers';
+import Script from 'next/script';
 
-// import Script from 'next/script';
 import Contact from '@/components/Contact/Contact';
 import Info from '@/components/Info/Info';
 import Title from '@/components/Title/Title';
 import Works from '@/components/Works/Works';
 
 export default async function Home() {
-	// const nonce = (await headers()).get('x-nonce') || '';
+	const nonce = (await headers()).get('x-nonce') || '';
 
 	return (
 		<>
-			{/* <Script src="https://www.googletagmanager.com/gtag/js" strategy="afterInteractive" nonce={nonce} /> */}
+			<Script src="https://www.googletagmanager.com/gtag/js" strategy="afterInteractive" nonce={nonce} />
 
 			<Contact showIndex={true} />
 			<Info />
